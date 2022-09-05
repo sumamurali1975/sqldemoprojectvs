bd766bb9fe2b7cd556cabf17b639c138f3a83ff5
sumamurali1975 <suma.palavekari@gmail.com>
2022-09-05 19:26:52 +0530

    First Commit

---
+++ Sqldemoproject/dbo/Tables/user_details.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[user_details] (
+    [user_id]    INT           NOT NULL,
+    [username]   VARCHAR (255) DEFAULT (NULL) NULL,
+    [first_name] VARCHAR (50)  DEFAULT (NULL) NULL,
+    [last_name]  VARCHAR (50)  DEFAULT (NULL) NULL,
+    [gender]     VARCHAR (10)  DEFAULT (NULL) NULL,
+    [password]   VARCHAR (50)  DEFAULT (NULL) NULL,
+    [status]     INT           DEFAULT (NULL) NULL,
+    PRIMARY KEY CLUSTERED ([user_id] ASC)
+);
+
